50d1188a9dfd185c62acb811b09c0c8604782422
iRosSoftware <iros0522@gmail.com>
2018-05-17 14:06:09 +0900

    Update getIDm.cpp
    コードの整理

--- getIDm.cpp
+++ getIDm.cpp
@@ -5,7 +5,8 @@
 int main()
 {
     RCS620S rcs620s;
-    
+
+    //初期化
     int ret = rcs620s.initDevice();
     if (!ret)
     {
@@ -13,22 +14,31 @@ int main()
         return 0;
     }
     
+    //読み取り部分
     char s[256] = {'\0'};
     do
     {
+        //ここで読み取る
         ret = rcs620s.polling();
-        
+
         if (ret)
         {
+            //idを取得
             uint8_t *idm = rcs620s.idm;
+            //変数sに出力
             sprintf(s, "%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x\n",
                     idm[0], idm[1], idm[2], idm[3],
                     idm[4], idm[5], idm[6], idm[7]);
         }
-    } while (printf(s) == 0);
+        
+    } //出力がなければ、繰り返し
+    while (strlen(s) == 0);
+    
+    //出力
     printf("%02x", s);
-    
+
+    //読み取りをオフにする
     rcs620s.rfOff();
-    
+
     return 0;
 }
